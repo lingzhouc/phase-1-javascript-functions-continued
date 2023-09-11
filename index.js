8704a03a3264a54530c61c47217e24852bdab6f8
LingZhouChen <lzcciao@gmail.com>
2023-09-11 19:42:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// saturdayFun activity w/ default roller-skate
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork activity w/ default go to the office
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// wrapAdjective("%")("a dedicated programmer"); 
+//=> "You are %a dedicated programmer%!"
+function wrapAdjective (flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
